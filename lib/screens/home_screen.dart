1c2277cdaff553ca5867d27ad767fe4fff43a153
Makai Pan <83694394+MakaiPan@users.noreply.github.com>
2022-03-18 16:37:06 +0800

    Add bottom navigation bar in home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/utils/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   static const id = 'home_screen';
@@ -9,8 +10,49 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentPageIndex = 0;
+  onPageChanged(int selectedPageIndex) {
+    setState(() {
+      _currentPageIndex = selectedPageIndex;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: kFooterColor,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        onTap: onPageChanged,
+        currentIndex: _currentPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.comment_bank,
+            ),
+            label: 'Meet & Char',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.lock_clock,
+            ),
+            label: 'Meetings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_outline,
+            ),
+            label: 'Contacts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.settings_outlined,
+            ),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
   }
 }
